497b8027b822d093dd069c129ad2899618600d8b
Vinicius da Silva <vinicius.silva.vortigo@gruposuperdigital.com>
2022-10-27 09:35:12 -0400

    add height to button

--- team-programming/ViewController.swift
+++ team-programming/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    private func setup(){}
 }
 
